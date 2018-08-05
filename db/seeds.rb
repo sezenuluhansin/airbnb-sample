fddff7228433b6973b529c0169a8bbfbc21efaf0
SEZEN ULUHANSIN <sezenuluhansin@hotmail.com>
2018-08-05 13:48:39 +0300

    editing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+)
+Flat.create!(
+  name: 'Historcal İstanbul',
+  address: 'eminönü no:11',
+  description: 'magical flat with a bosphorus view',
+  price_per_night: 176,
+)
+Flat.create!(
+  name: 'Times NY',
+  address: 'new york no:66',
+  description: 'citylights view in the center of nyc',
+  price_per_night: 300,
+)
+Flat.create!(
+  name: 'alphs mountains',
+  address: 'austria no:06',
+  description: 'close to the snowboarding area',
+  price_per_night: 200,
+)
+
+
