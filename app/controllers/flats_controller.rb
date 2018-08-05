15160cd23435f3bbf95a48e900193efbd18c4922
SEZEN ULUHANSIN <sezenuluhansin@hotmail.com>
2018-08-05 14:09:18 +0300

    editing 2

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,34 @@
 class FlatsController < ApplicationController
-  def index
-    @flats = Flat.all
+   def index
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name iLike '%#{params[:query]}%'")
+    else
+      @flats = Flat.all
+    end
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
 end
